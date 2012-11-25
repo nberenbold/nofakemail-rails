b3fe8dab9db8bc4aa84a0b08aaa271184c77d43c
Nils Berenbold <nils.berenbold@gmail.com>
2012-11-25 20:16:33 +0100

    removed readme

--- lib/nofakemail.rb
+++ lib/nofakemail.rb
@@ -3,13 +3,13 @@ require "open-uri"
 
 module Nofakemail
   class << self
-    def check_email input_email
+    def is_trashmail input_email
       body = open("http://nofakemail.net/webservice/isTrashmail/json?email=#{input_email}").read rescue ""
       json_body = ActiveSupport::JSON.decode body
       json_body["valid"]
     end
     
-    def check_emails email_array
+    def are_trashmails email_array
       email_array.each_with_index do |email,key|
         email_array[key] = "email[]=#{email}"
       end
